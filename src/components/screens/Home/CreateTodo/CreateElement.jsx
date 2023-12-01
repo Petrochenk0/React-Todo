ce91f3e19043af14b2194f52637ebeb60223d021
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2023-12-01 11:06:38 +0300

    Update CreateElement.jsx

--- src/components/screens/Home/CreateTodo/CreateElement.jsx
+++ src/components/screens/Home/CreateTodo/CreateElement.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 const CreateElement = ({ addTodo, setTodos }) => {
   const [title, setTitle] = useState("");
-
   console.log(title);
 
   const handleAddTodo = (title) => {
