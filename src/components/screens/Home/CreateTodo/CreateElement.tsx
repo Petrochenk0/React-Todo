cb40b300b6d351d4c4d942d257d69b791679a491
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2024-03-26 20:43:24 +0300

    TODO APPLICATION

--- src/components/screens/Home/CreateTodo/CreateElement.tsx
+++ src/components/screens/Home/CreateTodo/CreateElement.tsx
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, FC } from 'react';
 
-const CreateElement = ({ addTodo, setTodos }) => {
-  const [title, setTitle] = useState("");
+interface CreateElementProps {
+  addTodo: (todo: string) => void;
+  setTodos: React.Dispatch<React.SetStateAction<{ title: string; _id: string; isBool: boolean }[]>>;
+}
+
+const CreateElement: FC<CreateElementProps> = ({ addTodo, setTodos }) => {
+  const [title, setTitle] = useState<string>('');
   console.log(title);
-  const handleAddTodo = (title) => {
-    setTodos((prevTodos) => {
-      return [
-        {
-          title,
-          _id: Date.now().toString(),
-          isBool: false,
-        },
-        ...prevTodos,
-      ];
-    });
-    setTitle("");
+  const handleAddTodo = (title: string) => {
+    setTodos((prevTodos) => [
+      {
+        title,
+        _id: Date.now().toString(),
+        isBool: false,
+      },
+      ...prevTodos,
+    ]);
+    setTitle('');
   };
 
   return (
@@ -23,7 +26,7 @@ const CreateElement = ({ addTodo, setTodos }) => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
-        onKeyPress={(e) => e.key === "Enter" && handleAddTodo(title)}
+        onKeyPress={(e) => e.key === 'Enter' && handleAddTodo(title)}
         className="bg-transparent w-full outline-none font-bold border-none border-transparent"
         placeholder="Enter your todo"
       />
